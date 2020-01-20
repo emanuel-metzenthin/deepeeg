b2ae53a990e51efee96e3ec834f4bd8cd33ec1d3
Tobias Bredow <tobias.bredow@student.hpi.de>
2020-01-20 18:23:55 +0100

    helping the cnn to learn

--- deepeeg/deepeeg.py
+++ deepeeg/deepeeg.py
@@ -2,19 +2,22 @@ from deepeeg  import modelbuilder
 import logging
 from keras.models import model_from_json
 from keras.callbacks import EarlyStopping
+import keras
 
 class DeepEEG():
     # TODO make configurable
     # https://www.researchgate.net/publication/309873852_Single-trial_EEG_classification_of_motor_imagery_using_deep_convolutional_neural_networks
     # https://www.researchgate.net/publication/315096373_Deep_learning_with_convolutional_neural_networks_for_brain_mapping_and_decoding_of_movement-related_information_from_the_human_EEG
-    conv_layers = [{'filters': 25, 'pool_size': 3, 'kernel_size': 2, 'activation_func': 'relu'},
-                   {'filters': 50, 'pool_size': 3, 'kernel_size': 2, 'activation_func': 'relu'},
-                   {'filters': 100, 'pool_size': 2, 'kernel_size': 2, 'activation_func': 'relu'},
-                   {'filters': 200, 'pool_size': 2, 'kernel_size': 2, 'activation_func': 'relu'}]
-    dense_layers = [{'num_units': 1, 'activation_func': 'sigmoid'}]
+    conv_layers = [{'filters': 64, 'pool_size': 3, 'kernel_size': 3, 'activation_func': 'relu'},
+                   {'filters': 128, 'pool_size': 3, 'kernel_size': 3, 'activation_func': 'relu'},
+                   {'filters': 256, 'pool_size': 3, 'kernel_size': 3, 'activation_func': 'relu'},
+                   {'filters': 512, 'pool_size': 3, 'kernel_size': 3, 'activation_func': 'relu'}]
+    dense_layers = [{'num_units': 100, 'activation_func': 'relu'}, {'num_units': 1, 'activation_func': 'sigmoid'}]
 
     def train_cnn(self, X_train, y_train, X_val, y_val, save_model_to=None, save_weights_to=None):
         model = modelbuilder.build_cnn_model(self.conv_layers, self.dense_layers, (X_train.shape[1], X_train.shape[2]))
+
+        keras.optimizers.Adam(learning_rate=0.0001)
 
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -31,7 +34,7 @@ class DeepEEG():
 
         self.model = model
 
-        hist = model.fit(X_train, y_train, epochs=1000, validation_data=(X_val, y_val), callbacks=[EarlyStopping(min_delta=0.001)])
+        hist = model.fit(X_train, y_train, epochs=1000, validation_data=(X_val, y_val), batch_size=5)
 
         print(hist.history['accuracy'])
 
--- deepeeg/modelbuilder.py
+++ deepeeg/modelbuilder.py
@@ -1,14 +1,16 @@
 from keras import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Dense, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Dense, Flatten, BatchNormalization, Dropout
 
 
 def add_conv1D_layer(model, num_filters, pool_size=2, kernel_size=2, activation_func='relu', input_shape=''):
     if input_shape != '':
         model.add(Conv1D(filters=num_filters, kernel_size=kernel_size, activation=activation_func, input_shape=input_shape))
         model.add(MaxPooling1D(pool_size=pool_size))
+        model.add(BatchNormalization())
         return
     model.add(Conv1D(filters=num_filters, kernel_size=kernel_size,activation=activation_func))
     model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(BatchNormalization())
 
 def add_dense_layer(model, num_units, activation_func):
     model.add(Dense(num_units, activation=activation_func))
@@ -24,6 +26,7 @@ def build_cnn_model(conv_layers, dense_layers, input_shape):
             continue
         add_conv1D_layer(model, conv_layer['filters'], conv_layer['pool_size'], conv_layer['kernel_size'], conv_layer['activation_func'])
 
+    model.add(Dropout(rate=0.3))
     model.add(Flatten())
 
     for dense_layer in dense_layers:
