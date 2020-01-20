bec4a1227c4e78adec2ec0750e9791580d762269
Bredow <t.bredow@sap.com>
2020-01-20 13:18:04 +0100

    addind dataloader

--- deepeeg/main.py
+++ deepeeg/main.py
@@ -1,8 +1,10 @@
-from .argparser import parse_arguments
+#from .argparser import parse_arguments
+from dataloader import read_brainvis_from_directory
 
 
 def main():
-    arguments = parse_arguments()
+    arguments = read_brainvis_from_directory('./')
+    train,val = read_brainvis_from_directory('./')
     print(arguments)
 
 if __name__ == '__main__':
