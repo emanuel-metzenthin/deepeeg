dcde0cd8b71ea82a7486645eb098fd8a15c001a7
Tobias Bredow <tobias.bredow@student.hpi.de>
2020-01-27 16:47:23 +0100

    adding rnn support this time for real

--- deepeeg/modelbuilder.py
+++ deepeeg/modelbuilder.py
@@ -1,5 +1,5 @@
 from keras import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Dense, Flatten, BatchNormalization, Dropout
+from keras.layers import Conv1D, MaxPooling1D, Dense, Flatten, BatchNormalization, Dropout, LSTM
 
 
 def add_conv1D_layer(model, num_filters, pool_size=2, kernel_size=2, activation_func='relu', input_shape=''):
@@ -11,9 +11,6 @@ def add_conv1D_layer(model, num_filters, pool_size=2, kernel_size=2, activation_
     model.add(Conv1D(filters=num_filters, kernel_size=kernel_size,activation=activation_func))
     model.add(MaxPooling1D(pool_size=pool_size))
     model.add(BatchNormalization())
-
-def add_dense_layer(model, num_units, activation_func):
-    model.add(Dense(num_units, activation=activation_func))
     
 def build_cnn_model(conv_layers, dense_layers, input_shape):
     model = Sequential()
@@ -28,6 +25,30 @@ def build_cnn_model(conv_layers, dense_layers, input_shape):
 
     model.add(Dropout(rate=0.3))
     model.add(Flatten())
+
+    for dense_layer in dense_layers:
+        model.add(Dense(dense_layer['num_units'], activation=dense_layer['activation_func']))
+
+    return model
+
+
+def build_rnn_model(rnn_layers,dense_layers, input_shape):
+    model = Sequential()
+    first = True
+
+    for rnn_layer in rnn_layers:
+        if first:
+            if len(rnn_layers) == 1:
+                model.add(LSTM(rnn_layer['num_units'], input_shape=input_shape, return_sequences=False))
+                continue
+            model.add(LSTM(rnn_layer['num_units'], input_shape=input_shape, return_sequences=True))
+            first = False
+            continue
+        if rnn_layer == rnn_layers[len(rnn_layers)-1]:
+            model.add(LSTM(rnn_layer['num_units'], return_sequences=False))
+            continue
+        model.add(LSTM(rnn_layer['num_units'], return_sequences=True))
+
 
     for dense_layer in dense_layers:
         model.add(Dense(dense_layer['num_units'], activation=dense_layer['activation_func']))
